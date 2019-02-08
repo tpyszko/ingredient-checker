a5bbdc45af008080cfde77a0d100c09543f6629b
Tomek <tomasz.pyszko@gmail.com>
2019-02-08 14:29:05 +0100

    Prettier index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import App from "./container/App"
-import { ThemeProvider } from 'styled-components';
-import theme from "./theme/standard-theme"
-import GlobalStyle from "./theme/global-style"
+import App from "./container/App";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme/standard-theme";
+import GlobalStyle from "./theme/global-style";
 
 const AppProvider = () => (
-    <ThemeProvider theme={theme}>
-        <Fragment>
-            <App />
-            <GlobalStyle />
-        </Fragment>
-    </ThemeProvider>
+  <ThemeProvider theme={theme}>
+    <Fragment>
+      <App />
+      <GlobalStyle />
+    </Fragment>
+  </ThemeProvider>
 );
 
 const wrapper = document.getElementById("root");
