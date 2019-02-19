9abc75c759bfcc2811c09fa6e0e7d47a51e787a9
Tomek <tomasz.pyszko@gmail.com>
2019-02-19 14:43:54 +0100

    Add actions, reducers and constants

---
+++ src/actions/ingredients.js
@@ -0,0 +1,123 @@
+import axios from "axios";
+import { ocr_response_format } from "../helpers/helpers";
+import { find_harmful_ingredients } from "../helpers/harmful_ingredients_list";
+
+import {
+  HANDLE_UPLOAD_IMAGE_BEGIN,
+  HANDLE_UPLOAD_IMAGE_SUCCESS,
+  HANDLE_UPLOAD_IMAGE_FAILURE,
+  HANDLE_SUBMIT_IMAGE_BEGIN,
+  HANDLE_SUBMIT_IMAGE_SUCCESS,
+  HANDLE_SUBMIT_IMAGE_FAILURE,
+  HANDLE_CLEAR_STATE
+} from "../actions/constants";
+
+// ============ UPLOADING IMAGE ==================
+export const handleUploadImageSuccess = image => {
+  return {
+    type: HANDLE_UPLOAD_IMAGE_SUCCESS,
+    image
+  };
+};
+
+export const handleUploadImageBegin = () => {
+  return {
+    type: HANDLE_UPLOAD_IMAGE_BEGIN
+  };
+};
+
+export const handleUploadImageFailure = error => {
+  return {
+    type: HANDLE_UPLOAD_IMAGE_FAILURE,
+    error
+  };
+};
+
+export const handleUploadImage = event => {
+  return async dispatch => {
+    dispatch(handleUploadImageBegin());
+    dispatch(handleClearState());
+    try {
+      const reader = new FileReader();
+      const file = event.target.files[0];
+      reader.onloadend = () =>
+        dispatch(handleUploadImageSuccess(reader.result));
+      reader.readAsDataURL(file);
+    } catch (error) {
+      dispatch(handleUploadImageFailure(["Please upload image"]));
+    }
+  };
+};
+
+// ============ SUBMITING IMAGE ==================
+export const handleSubmitImageSuccess = e => {
+  return {
+    type: HANDLE_SUBMIT_IMAGE_SUCCESS,
+    e
+  };
+};
+
+export const handleSubmitImageBegin = () => {
+  return {
+    type: HANDLE_SUBMIT_IMAGE_BEGIN
+  };
+};
+
+export const handleSubmitImageFailure = error => {
+  return {
+    type: HANDLE_SUBMIT_IMAGE_FAILURE,
+    error
+  };
+};
+
+export const handleOnSubmitAction = image => {
+  return dispatch => {
+    dispatch(handleUploadImageBegin());
+    const img = image;
+    let formData = new FormData();
+    formData.append("base64image", img);
+    formData.append("language", "eng");
+    formData.append("apikey", "cd7318c36b88957");
+
+    axios({
+      method: "post",
+      url: "https://api.ocr.space/parse/image",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data: formData
+    })
+      .then(response => {
+        let parse_response = JSON.parse(response.request.responseText);
+        let error_message_array = parse_response.ErrorMessage
+          ? parse_response.ErrorMessage
+          : null;
+
+        if (error_message_array) {
+          dispatch(handleSubmitImageFailure(error_message_array));
+        } else {
+          let ingredients_array = ocr_response_format(
+            parse_response.ParsedResults[0].ParsedText
+          );
+          let harmful_ingredients_array = find_harmful_ingredients(
+            ingredients_array
+          );
+          dispatch(handleSubmitImageSuccess(harmful_ingredients_array));
+        }
+      })
+      .catch(error => {
+        dispatch(
+          handleSubmitImageFailure([
+            "We couldnt check it for you, try it later"
+          ])
+        );
+      });
+  };
+};
+
+// ============  CLEAR STATE ==================
+export const handleClearState = () => {
+  return {
+    type: HANDLE_CLEAR_STATE
+  };
+};
