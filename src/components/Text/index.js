047a8748424184a7e740a223307bdf401cea50c6
Tomek <tomasz.pyszko@gmail.com>
2019-03-22 13:50:23 +0100

    Add deploy script to package.json

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Text = styled.p`
   color: ${({ color, theme }) => (color ? color : theme.color.dark)};
-  color: ${props => (props.primary ? "palevioletred" : "white")};
   font-size: ${({ fontSize }) => fontSize};
   margin: 0.3em 0;
   line-height: 21px;
